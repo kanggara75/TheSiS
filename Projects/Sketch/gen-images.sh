9f4e59c973622e9b3a04e90e27f77244352fc122
KAnggara75 <KAnggara75@gmail.com>
2019-12-19 11:20:56 +0700

    first commit

---
+++ Projects/Sketch/gen-images.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Cleaning up old PNGs..."
+rm images/*.png
+
+echo "Renaming SVGs..."
+mv images/breakout.svg images/schematic.svg
+mv images/breakout.sch.svg images/schematic.svg
+mv images/breakout-brd.svg images/board.svg
+
+shopt -s nullglob
+for f in images/*.svg
+do
+    fname=$(basename "$f" ".svg")
+    echo "Converting $fname.svg..."
+    convert -density 1600 -flatten -resize 1500x1500 images/$fname.svg images/$fname.png
+
+    echo "Quantizing image..."
+    pngquant 256 images/$fname.png
+
+    echo "Renaming..."
+    mv images/$fname-fs8.png images/$fname.png
+done
---
+++ README.md
@@ -0,0 +1,16 @@
+A6-ESP8266-breakout
+===================
+
+This is a breakout board that includes an ESP8266 and an integrated A6 module.
+It is best used with the [A6 ESP8266
+library](https://github.com/skorokithakis/A6lib).
+
+It is still incomplete, and looks like this:
+
+## Schematic
+
+<img src="Projects/Sketch/images/schematic.png" />
+
+## Board
+
+<img src="Projects/Sketch/images/board.png" />
