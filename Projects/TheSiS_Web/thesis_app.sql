a67bea64f33ae72e8c78a7082e18c23085c256ed
KAnggara75 [Windows X] <kanggara75@gmail.com>
2020-10-04 01:39:14 +0700

    Add database untuk thesis web

---
+++ Projects/TheSiS_Web/thesis_app.sql
@@ -0,0 +1,434 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 05, 2020 at 06:43 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `thesis_app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `acc`
+--
+
+CREATE TABLE `acc` (
+  `id` int(10) NOT NULL,
+  `time` timestamp(2) NOT NULL DEFAULT current_timestamp(2),
+  `x` int(10) NOT NULL,
+  `y` int(10) NOT NULL,
+  `z` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `acc`
+--
+
+INSERT INTO `acc` (`id`, `time`, `x`, `y`, `z`) VALUES
+(1, '2020-06-19 02:08:52.00', 1, 1024, 256),
+(2, '2020-06-19 02:11:52.00', 2, 512, 512),
+(3, '2020-06-19 02:11:55.00', 4, 256, 512),
+(4, '2020-06-19 02:12:40.00', 8, 128, 256),
+(5, '2020-06-19 02:12:40.00', 16, 64, 1024),
+(6, '2020-06-19 02:13:39.00', 32, 32, 128),
+(7, '2020-06-19 02:13:39.00', 64, 16, 512),
+(8, '2020-06-19 02:14:29.00', 128, 8, 512),
+(9, '2020-06-19 02:14:29.00', 256, 4, 1024),
+(10, '2020-06-19 02:15:25.00', 512, 2, 1024),
+(11, '2020-06-19 02:15:25.00', 1024, 1, 1024),
+(12, '2020-06-19 02:16:59.00', 32, 32, 256);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `boards`
+--
+
+CREATE TABLE `boards` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `board` int(6) DEFAULT NULL,
+  `last_request` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `boards`
+--
+
+INSERT INTO `boards` (`id`, `board`, `last_request`) VALUES
+(1, 1, '2020-06-30 22:24:42');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kontrol`
+--
+
+CREATE TABLE `kontrol` (
+  `id` int(2) NOT NULL,
+  `board` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `gpio` int(11) NOT NULL,
+  `state` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kontrol`
+--
+
+INSERT INTO `kontrol` (`id`, `board`, `nama`, `gpio`, `state`) VALUES
+(1, 0, 'GPS', 0, 1),
+(2, 0, 'Alarm', 0, 1),
+(3, 0, 'Listrik', 0, 1),
+(4, 0, 'Mesin', 1, 1),
+(5, 1, 'Notif', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kuisioner`
+--
+
+CREATE TABLE `kuisioner` (
+  `no` int(3) NOT NULL,
+  `time` int(10) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `jk` int(1) NOT NULL,
+  `usia` int(1) NOT NULL,
+  `motor` int(1) NOT NULL,
+  `A1` int(1) NOT NULL,
+  `A2` int(1) NOT NULL,
+  `A3` int(1) NOT NULL,
+  `A4` int(1) NOT NULL,
+  `A5` int(1) NOT NULL,
+  `B1` int(1) NOT NULL,
+  `B2` int(1) NOT NULL,
+  `B3` int(1) NOT NULL,
+  `B4` int(1) NOT NULL,
+  `B5` int(1) NOT NULL,
+  `C1` int(1) NOT NULL,
+  `C2` int(1) NOT NULL,
+  `C3` int(1) NOT NULL,
+  `C4` int(1) NOT NULL,
+  `C5` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kuisioner`
+--
+
+INSERT INTO `kuisioner` (`no`, `time`, `name`, `jk`, `usia`, `motor`, `A1`, `A2`, `A3`, `A4`, `A5`, `B1`, `B2`, `B3`, `B4`, `B5`, `C1`, `C2`, `C3`, `C4`, `C5`) VALUES
+(1, 1590980342, 'Kelvin Anggara', 1, 1, 1, 1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4),
+(2, 1590980463, 'Kelvin Anggara gtg', 2, 18, 2, 1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5),
+(3, 1591006043, 'kak', 1, 1111, 1, 1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4),
+(4, 1591006119, 'kak', 1, 1111, 1, 1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `outputs`
+--
+
+CREATE TABLE `outputs` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `name` varchar(64) DEFAULT NULL,
+  `board` int(6) DEFAULT NULL,
+  `gpio` int(6) DEFAULT NULL,
+  `state` int(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `outputs`
+--
+
+INSERT INTO `outputs` (`id`, `name`, `board`, `gpio`, `state`) VALUES
+(1, 'Built-in LED', 1, 2, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `image` varchar(128) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `is_active` int(1) NOT NULL,
+  `date_created` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `image`, `password`, `role_id`, `is_active`, `date_created`) VALUES
+(1, 'Kelvin Anggara', 'kanggara75@gmail.com', 'LogoKA20201.jpg', '$2y$10$qMINsDbz7WvhH8yjUgggPe6ntn8E7dDGNEikCz5cWY11q0q7tk5uO', 1, 1, 1586930611),
+(5, 'KAnggara', 'KAnggara75@yandex.com', 'default1.jpg', '$2y$10$fn/jTUfEs57jxzaA9NZQf.nw7XrL0yXtH/pLWaMbK14oZ2qa97h/q', 2, 1, 1587031472),
+(6, 'Kelvin', 'kanggara751@gmail.com', 'default.jpg', '$2y$10$S0gRqy2qaoS9PoHQW0YfTuoLnxfZLBa3CPnCbD93DNj8ZP5ANXdsO', 2, 1, 1590979986);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_access_menu`
+--
+
+CREATE TABLE `user_access_menu` (
+  `id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_access_menu`
+--
+
+INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
+(1, 1, 1),
+(3, 2, 2),
+(9, 2, 3),
+(10, 1, 2),
+(11, 1, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_menu`
+--
+
+CREATE TABLE `user_menu` (
+  `id` int(11) NOT NULL,
+  `menu` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_menu`
+--
+
+INSERT INTO `user_menu` (`id`, `menu`) VALUES
+(1, 'Admin'),
+(2, 'User'),
+(3, 'Menu'),
+(4, 'Test');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role`
+--
+
+CREATE TABLE `user_role` (
+  `id` int(11) NOT NULL,
+  `role` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_role`
+--
+
+INSERT INTO `user_role` (`id`, `role`) VALUES
+(1, 'Admin'),
+(2, 'Member');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_sub_menu`
+--
+
+CREATE TABLE `user_sub_menu` (
+  `id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `url` varchar(128) NOT NULL,
+  `icon` varchar(128) NOT NULL,
+  `is_active` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_sub_menu`
+--
+
+INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active`) VALUES
+(1, 1, 'Admin Page', 'admin', 'fas fa-fw fa-tachometer-alt', 1),
+(2, 2, 'My Profile', 'user', 'fas fa-fw fa-user', 1),
+(3, 2, 'Edit Profile', 'user/edit', 'fas fa-fw fa-user-edit', 1),
+(4, 3, 'Menu Management', 'menu', 'fas fa-fw fa-folder', 1),
+(5, 3, 'Sub Menu Management', 'menu/submenu', 'fas fa-fw fa-folder-open', 1),
+(6, 1, 'Role', 'admin/role', 'fas fa-fw fa-user-tie', 1),
+(7, 2, 'Change Password', 'user/cp', 'fas fa-fw fa-key', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_token`
+--
+
+CREATE TABLE `user_token` (
+  `id` int(10) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `token` varchar(128) NOT NULL,
+  `date` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `acc`
+--
+ALTER TABLE `acc`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `boards`
+--
+ALTER TABLE `boards`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kontrol`
+--
+ALTER TABLE `kontrol`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kuisioner`
+--
+ALTER TABLE `kuisioner`
+  ADD PRIMARY KEY (`no`);
+
+--
+-- Indexes for table `outputs`
+--
+ALTER TABLE `outputs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_menu`
+--
+ALTER TABLE `user_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_token`
+--
+ALTER TABLE `user_token`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `acc`
+--
+ALTER TABLE `acc`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `boards`
+--
+ALTER TABLE `boards`
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `kontrol`
+--
+ALTER TABLE `kontrol`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `kuisioner`
+--
+ALTER TABLE `kuisioner`
+  MODIFY `no` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `outputs`
+--
+ALTER TABLE `outputs`
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `user_menu`
+--
+ALTER TABLE `user_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user_role`
+--
+ALTER TABLE `user_role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `user_token`
+--
+ALTER TABLE `user_token`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
